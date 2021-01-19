9a33aaa1262b8ed8dd969359867a7218df02b08e
Tobias Lindberg <tobias.lindberg@ga.ntig.se>
2021-01-19 15:25:32 +0100

    7.5.1 färdigt

--- Player.cs
+++ Player.cs
@@ -55,19 +55,4 @@ namespace SpaceShooter
         }
     }
 
-    class GoldCoin : PhysicalObject
-    {
-        double timeToDie; //Hur länge guldmyntet lever kvar.
-
-        public GoldCoin(Texture2D image, float X, float Y, GameTime gameTime) : base(image, X, Y, 0, 2f)
-        {
-            timeToDie = gameTime.TotalGameTime.TotalMilliseconds + 5000;
-        }
-
-        public void Update(GameTime gameTime)
-        {
-            if (timeToDie < gameTime.TotalGameTime.TotalMilliseconds)
-                IsAlive = false;
-        }
-    }
 }
--- PowerUp.cs
+++ PowerUp.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace SpaceShooter
+{
+    class GoldCoin : PhysicalObject
+    {
+        double timeToDie; //Hur länge guldmyntet lever kvar.
+
+        public GoldCoin(Texture2D image, float X, float Y, GameTime gameTime) : base(image, X, Y, 0, 2f)
+        {
+            timeToDie = gameTime.TotalGameTime.TotalMilliseconds + 5000;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (timeToDie < gameTime.TotalGameTime.TotalMilliseconds)
+                IsAlive = false;
+        }
+    }
+
+}
